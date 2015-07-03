f819a31b7460b3f0593d7104bcdff2e7e78a312e
Enrico Antonello <eantonello@h-art.com>
2015-07-03 16:57:21 +0200

    post

--- test_telegram_bot.php
+++ test_telegram_bot.php
@@ -86,6 +86,7 @@ include 'Crawler.php';
 
             try {
                 //$result = $this->httpGet(sprintf($this->updates_api, $this->getToken(), $offset));
+                die(var_dump($_POST));
                 $array_res = json_decode($_POST, true);
 
                 if($array_res['ok']) {
